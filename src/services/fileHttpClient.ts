5079fe66cd099cddae340d57d2203a11f0676b54
srahman3311 <samsur.rahman3311@gmail.com>
2025-04-06 00:18:53 +0600

    Add withCredentials: true to fileHttpClient for files uploading to work

--- src/services/fileHttpClient.ts
+++ src/services/fileHttpClient.ts
@@ -3,7 +3,8 @@ import Cookies from "universal-cookie";
 
 const fileHttpClient = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_FILE_UPLOAD_URL,
-    timeout: 5000
+    timeout: 5000,
+    withCredentials: true
 });
 
 fileHttpClient.interceptors.request.use((config) => {
